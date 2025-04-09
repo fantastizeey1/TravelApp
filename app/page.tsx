32c7fd132e7f8301c6f67d8729fc4f1713932137
Ayoola Enitan Prevail <137091141+fantastizeey1@users.noreply.github.com>
2025-04-09 22:05:17 +0100

    Update page.tsx
    Testing widget

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
+import { useEffect } from "react";
 import Camp from "@/components/Camp";
 import Features from "@/components/Features";
 import GetApp from "@/components/GetApp";
 import Guide from "@/components/Guide";
 import Hero from "@/components/Hero";
-import Image from "next/image";
 
 export default function Home() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://plugin.aktok.com/aktokwsc-widget.js";
+    script.async = true;
+    document.body.appendChild(script);
+  }, []);
+
   return (
     <>
       <Hero />
@@ -13,6 +20,12 @@ export default function Home() {
       <Guide />
       <Features />
       <GetApp />
+
+      {/* Mounting the widget */}
+      <div>
+        <div dangerouslySetInnerHTML={{ __html: '<aktokwsc-widget id="aktokwsc" uuid="77adbde1-3b80-4599-892c-cfccb0444b6c"></aktokwsc-widget>' }} />
+      </div>
     </>
   );
 }
+
